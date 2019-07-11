1caba81a4cd2aba6a6a29e85869d6b5add99bc74
Zebulun Arendsee <arendsee@iastate.edu>
2019-07-11 11:04:12 -0500

    Implement inferTypes with newvar
    I can now parse a RawExpr into a typed expression where every variable
    is given its own generic type. There is no type inference here, just
    type naming. For example:
    
     * Variable
        foo  ==>  foo:x0
     * Application
        foo x  ==>  (foo:x0 x:x1):x2
     * Abstraction
        \x -> y x ==> (\x . (y:x0 x:x1):x2):x3
     * Let
        let x = f y in h x ==> (let x = (f:x1 y:x2) in (h:x3 x:x4):x5):x6
    
    Note that every expression has a type variable associated with it.
    Abstraction and Let both bind a variable, which is not itself given a
    type.
    
    Next we can infer relationships between the named type variables.
    
     1. In the application:
          foo x  ==>  (foo:x0 x:x1):x2
        we can infer
          x0 |- (x1 -> x2)
        thus
          (foo:x0 x:x1):x2 ==> (foo:(x1->x2) x:x1):x2
        This eliminates 1 type variable
    
     2. In Abstraction and Let we can set each occurence of a bound variable
     to a new variable name. For example in:
            \x -> y x x ==> (\x . ((y:x0 x:x1) x:x2):x3):x4
        We can infer:
            (\x . ((y:x0 x:x1):x2 x:x3):x4):x5 ==> (\x . ((y:x0 x:x1):x2 x:x1):x4):x5
        This eliminates x3
        We can continue as well by inferring within the application
            (\x . ((y:x0 x:x1):x2 x:x1):x4):x5
              ==> (\x . ((y:(x1->x2) x:x1):x2 x:x1):x4):x5
        And again:
              (\x . ((y:(x1->x2) x:x1):x2 x:x1):x4):x5
                ==> (\x . ((y:(x1->(x1->x4)) x:x1):(x1->x4) x:x1):x4):x5
    
    The next step is to implement these inference cases. When replacing
    bound characters I will need to ensure that they are free in the
    expression (e.g. variable names in deeper abstractions and let
    expressions may shadow higher names).

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main where
 
 import Lib
+import qualified Control.Monad.State as MS
+
+-- x
+e0 = V "foo"
+
+-- foo x
+e1 = A (V "foo") (V "x")
+
+-- foo x y
+e2 = A (A (V "foo") (V "x")) (V "y")
+
+-- \ x . y x
+e3 = S "x" (A (V "y") (V "x"))
+
+-- let x = f y in h x
+e4 = L "x" (A (V "f") (V "y")) (A (V "h") (V "x"))
 
 main :: IO ()
-main = print (TVar "hi")
+main = do
+  print $ MS.evalState (inferTypes e0) initJState
+  print $ MS.evalState (inferTypes e1) initJState
+  print $ MS.evalState (inferTypes e2) initJState
+  print $ MS.evalState (inferTypes e3) initJState
+  print $ MS.evalState (inferTypes e4) initJState
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,35 +1,50 @@
 module Lib (
     MonoType(..)
   , PolyType(..)
+  , RawExpr(..)
+  , Expr(..)
+  , inferTypes
   , newvar
-
+  , initJState
 ) where
 
 import qualified Data.Set as S
 import qualified Control.Monad.State as MS
 
--- infixl 1 |>>
--- (|>>) :: Functor f => f a -> (a -> b) -> f b
--- (|>>) = flip fmap
+infixl 1 |>>
+(|>>) :: Functor f => f a -> (a -> b) -> f b
+(|>>) = flip fmap
 
 type Name = String
 
--- data NudeExpr
---   = NudeEVar Name           -- Variable
---   | NudeAppl Expr Expr      -- Application
---   | NudeAbst Name Expr      -- Abstraction
---   | NudeLet Name Expr Expr  -- Let
---
--- data Expr
---   = EVar Name MonoType          -- Variable
---   | Appl Expr Expr MonoType     -- Application
---   | Abst Name Expr MonoType     -- Abstraction
---   | Let Name Expr Expr MonoType -- Let
+data RawExpr
+  = V Name                 -- Variable
+  | A RawExpr RawExpr      -- Application
+  | S Name RawExpr         -- Abstraction
+  | L Name RawExpr RawExpr -- Let
+  deriving(Show, Eq, Ord)
+
+data Expr
+  = EVar Name MonoType          -- Variable
+  | Appl Expr Expr MonoType     -- Application
+  | Abst Name Expr MonoType     -- Abstraction
+  | Let Name Expr Expr MonoType -- Let
+  deriving(Eq, Ord)
+
+instance Show Expr where
+  show (EVar n t) = n ++ ":" ++ show t
+  show (Appl e1 e2 t) = "(" ++ show e1 ++ " " ++ show e2 ++ "):" ++ show t
+  show (Abst n e t) = "(\\" ++ n ++ " . " ++ show e ++ "):" ++ show t 
+  show (Let n e1 e2 t) = "(let " ++ n ++ " = " ++ show e1 ++ " in " ++ show e2 ++ ":)" ++ show t
 
 data MonoType
   = TVar Name
   | TAppl MonoType MonoType
-  deriving(Show, Eq, Ord)
+  deriving(Eq, Ord)
+
+instance Show MonoType where
+  show (TVar n) = n
+  show (TAppl t1 t2) = show t1 ++ " " ++ show t2  
 
 data PolyType
   = PVar MonoType
@@ -51,115 +66,26 @@ initJState = HMState {
     , index = 0
   }
 
-newvar :: J Name
+newvar :: J MonoType
 newvar = do
   s <- MS.get
-  let v = "x" ++ show (index s)
+  let v = TVar $ "x" ++ show (index s)
   MS.put (s {index = index s + 1})
   return v
 
-
--- inst :: PolyType -> J MonoType
--- inst = inst' [] where
---   inst' :: [(Name, Name)] -> PolyType -> J MonoType
---   inst' xs (ForAll n t) = newvar >>= (\v -> inst' ((n, v):xs) t)
---   inst' xs (PVar t) = return t
---
---   instMono :: [(Name, Name)] -> MonoType -> MonoType
---   instMono xs (TVar n) = maybe (TVar n) TVar (lookup n xs)
---   instMono xs (TAppl t1 t2) = TAppl (instMono xs t1) (instMono xs t2)
-
---
--- data Typing = Typing Context Expr MonoType
---
--- class Typed a where
---   free :: a -> S.Set MonoType
---
--- instance Typed Typing where
---   free (Typing context _ polytype) = free polytype \\ free context
---
--- instance Typed Context where
---   free (Context xs) = S.fromList $ mapM (free . snd) xs
---
--- instance Typed PolyType where
---   free (PVar t) = free t
---   free (ForAll n t) = S.delete n (free t)
---
--- instance Typed MonoType where
---   free (TVar n) = S.singleton n
---   free (TAppl xs) = S.fromList $ mapM free xs
---
--- data Predicate
---   = IsSubset PolyType PolyType
---   | IsBound Name Context
---   | IsDefined Name Name Context
---
--- data Premise
---   = PPredicate Predicate
---   | PTyping Typing
---
--- type Judgement = Typing
--- type Conclusion = Judgement
-
--- addContext :: (Name,Name) -> J ()
--- addContext c = do
---   s <- MS.get
---   MS.put (s {context = c : context s})
---
--- tag :: MonoType -> NudeExpr -> Expr
--- tag (NudeEVar n) t = EVar t n
--- tag (NudeAppl n e) t = Appl t n e
--- tag (NudeAbst n e) t = Abst t n e
--- tag (NudeLet n e1 e2) t = Let t n e1 e2
---
--- -- data Expr
--- --   = EVar Name MonoType          -- Variable
--- --   | Appl Expr Expr MonoType     -- Application
--- --   | Abst Name Expr MonoType     -- Abstraction
--- --   | Let Name Expr Expr MonoType -- Let
--- --
--- -- data MonoType
--- --   = TVar Name
--- --   | TAppl MonoType MonoType
--- --   deriving(Show, Eq, Ord)
--- --
--- -- data PolyType
--- --   = PVar MonoType
--- --   | ForAll Name PolyType
--- --   deriving(Show, Eq, Ord)
---
--- inferTypes :: NudeExpr -> J Expr
--- inferTypes (NudeEVar n) = newvar |>> EVar n
--- inferTypes (NudeAppl e1 e2) = do
---   t1' <- inferTypes e1
---   t2' <- inferTypes e1
---   t <- newvar
---   let u = unify t
---   return u
---
--- unify :: MonoType -> MonoType -> Expr -> Expr
--- unify (TVar n1) (TVar n2) = replace (TVar n1) (TVar n2)
--- unify (TAppl n1 m1) (TAppl n2 m2) = undefined
---   -- | len m1 == len m2 && n1 == n2 =
---
--- replace :: MonoType -> MonoType -> Expr -> Expr
--- replace a b = mapMonoType replace' where
---   replace' (EVar n t)
---     | a == t = EVar n b
---     | otherwise = EVar n a
---   replace' (TAppl e1 e2) = TAppl (replace a b e1) (replace a b e2)
---
--- replace a b (EVar n t)
---   | a == t = EVar n b
---   | otherwise = EVar n a
--- replace a b (TAppl e1 e2) = TAppl (replace a b e1) (replace a b e2)
---
--- mapMonoType :: (MonoType -> MonoType) -> Expr -> Expr
--- mapMonoType f (EVar n t) = EVar n (f t)
--- mapMonoType f (Appl e1 e2 t) = Appl (mapMonoType f e1) (mapMonoType f e2) (f t)
--- mapMonoType f (Abst n e t) = Abst n (mapMonoType  f e) (f t)
---
---
--- len :: MonoType -> Int
--- len (TVar _) = 1
--- len (TAppl _ m) = 1 + len m
+inferTypes :: RawExpr -> J Expr
+inferTypes (V n) = newvar |>> EVar n
+inferTypes (A e1 e2) = do
+  t' <- newvar
+  e1' <- inferTypes e1
+  e2' <- inferTypes e2
+  return $ Appl e1' e2' t'
+inferTypes (S n e) = do
+  t' <- newvar
+  e' <- inferTypes e
+  return $ Abst n e' t'
+inferTypes (L n e1 e2) = do
+  t' <- newvar
+  e1' <- inferTypes e1
+  e2' <- inferTypes e2
+  return $ Let n e1' e2' t'
