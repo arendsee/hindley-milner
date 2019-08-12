6ceb22787ab20582df501163fbd9032de15ded4a
Zebulun Arendsee <arendsee@iastate.edu>
2019-08-12 08:27:22 -0500

    Add docopt-based executable
    Also I removed all the messy old main testing

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,32 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
-import DkTest
+import System.Console.Docopt
+import Control.Monad (when)
+import qualified System.Environment as SE
+import Bidirectional.Dunfield.Infer
+import Bidirectional.Dunfield.Data
+import Bidirectional.Dunfield.Parser
+import Data.Text.Prettyprint.Doc (pretty)
 
--- run :: Expr -> Stack Gamma
--- run expr = MS.runStateT (MW.runWriterT (ME.runExceptT( infer [] expr ))) 0
+template :: Docopt
+template = [docoptFile|USAGE|]
 
+getArgOrExit :: Arguments -> Option -> IO String
+getArgOrExit = getArgOrExitWith template
 
 main :: IO ()
-main = runDkTest
+main = do
+  args <- parseArgsOrExit template =<< SE.getArgs
+  let verbosityStr = getArgWithDefault args "1" (longOption "verbosity")
+  expr <- getArgOrExit args (argument "expression")
+  case (reads verbosityStr :: [(Int, String)]) of
+    [(verbosity, "")] -> do
+      t <- runStack (typecheck (readExpr expr)) verbosity
+      if verbosity > 0
+      then case t of
+        (Right t) -> print (pretty t)
+        (Left err) -> print (pretty err)
+      else return ()
+    _ -> error "Expected verbosity argument to be an integer"
