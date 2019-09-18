c211b0e5077bcc0d8acbe1d96a74812a799c94c6
Zebulun Arendsee <arendsee@iastate.edu>
2019-09-18 07:28:19 -0500

    Clean up tests

--- test-suite/UnitTests.hs
+++ test-suite/UnitTests.hs
@@ -27,38 +27,39 @@ typeof es = typeof' . head . reverse $ es where
   typeof' (AppE _ t) = typeof' t
   typeof' t = error ("No annotation found for: " <> show t)
 
-exprTestGood :: T.Text -> Type -> TestTree
-exprTestGood e t
-  = testCase (T.unpack e)
-  $ case runStack (typecheck (readProgram e)) of
+exprTestGood :: String -> T.Text -> Type -> TestTree
+exprTestGood msg code t
+  = testCase msg
+  $ case runStack (typecheck (readProgram code)) of
       (Right es', _) -> assertEqual "" t (typeof (main es'))
-      (Left err, _) -> error (show err)
+      (Left err, _) -> error $  "The following error was raised: " <> show err
+                             <> "\nin:\n" <> show code
 
-exprTestFull :: T.Text -> T.Text -> TestTree
-exprTestFull code expCode
-  = testCase (T.unpack code)
+exprTestFull :: String -> T.Text -> T.Text -> TestTree
+exprTestFull msg code expCode
+  = testCase msg
   $ case runStack (typecheck (readProgram code)) of
       (Right e, _) -> assertEqual "" (main e) (main $ readProgram expCode) 
       (Left err, _) -> error (show err)
 
-exprTestBad :: T.Text -> TestTree
-exprTestBad e
-  = testCase ("Fails?: " <> T.unpack e)
+exprTestBad :: String -> T.Text -> TestTree
+exprTestBad msg e
+  = testCase msg
   $ case runStack (typecheck (readProgram e)) of
       (Right _, _) -> assertFailure . T.unpack $ "Expected '" <> e <> "' to fail"
       (Left _, _) -> return ()
 
-expectError :: T.Text -> TypeError -> TestTree
-expectError expr err = testCase ("Fails?: " <> T.unpack expr)
+expectError :: String -> T.Text -> TypeError -> TestTree
+expectError msg expr err = testCase msg
   $ case runStack (typecheck (readProgram expr)) of
       (Right _, _) -> assertFailure . T.unpack $ "Expected failure"
       (Left err, _) -> return ()
       (Left err', _) -> assertFailure
         $ "Expected error (" <> show err <> ") got error (" <> show err' <> ")"
 
-testPasses :: T.Text -> TestTree
-testPasses e
-  = testCase ("Fails?: " <> T.unpack e)
+testPasses :: String -> T.Text -> TestTree
+testPasses msg e
+  = testCase msg
   $ case runStack (typecheck (readProgram e)) of
       (Right _, _) -> return ()
       (Left e, _) -> assertFailure $ "Expected this test to pass, but it failed with the message: " <> show e
@@ -80,162 +81,189 @@ tuple ts = ArrT v ts where
 
 unitTests = testGroup "Unit tests"
   [
-      exprTestGood "42" int
-    , exprTestGood "True" bool
-    , exprTestGood "4.2" num
-    , exprTestGood "\"this is a string literal\"" str
-    , exprTestGood "42 :: Int" int
-    , exprTestGood "True :: Bool" bool
-    , exprTestGood "4.2 :: Num" num
-    , exprTestGood "\"this is a string literal\" :: Str" str
-    , exprTestGood "(4.2, True)" (arr "Tuple2" [num, bool])
-    , exprTestGood "f :: Int -> Int; f (42 :: Int)" int
-    , exprTestGood "f x = x; f 42" int
-    , exprTestGood "snd x y = y; snd True 42" int
-    , exprTestGood "(\\x -> True)" (forall ["a"] (fun [var "a", bool]))
-    , exprTestGood "(\\x -> True) 42" bool
-    , exprTestGood "(\\x -> (\\y -> True) x) 42" bool
-    , exprTestGood "(\\x -> (\\y -> x) True) 42" int
-    , exprTestGood "(\\x y->x) 1 True" int
-    , exprTestGood "(\\x y -> x) :: forall a b . a -> b -> a"
+    -- primitives
+      exprTestGood "primitive integer" "42" int
+    , exprTestGood "primitive boolean" "True" bool
+    , exprTestGood "primitive double" "4.2" num
+    , exprTestGood "primitive string" "\"this is a string literal\"" str
+    , exprTestGood "primitive integer annotation" "42 :: Int" int
+    , exprTestGood "primitive boolean annotation" "True :: Bool" bool
+    , exprTestGood "primitive double annotation" "4.2 :: Num" num
+    , exprTestGood "primitive string annotation" "\"this is a string literal\" :: Str" str
+    , exprTestGood "primitive declaration" "x = True; 4.2" num
+    -- declarations
+    , exprTestGood "identity function declaration and application" "f x = x; f 42" int
+    , exprTestGood "snd function declaration and application" "snd x y = y; snd True 42" int
+    , exprTestGood "explicit annotation within an application" "f :: Int -> Int; f (42 :: Int)" int
+    -- lambdas
+    , exprTestGood "fully applied lambda (1)" "(\\x y -> x) 1 True" int
+    , exprTestGood "fully applied lambda (2)" "(\\x -> True) 42" bool
+    , exprTestGood "fully applied lambda (3)" "(\\x -> (\\y -> True) x) 42" bool
+    , exprTestGood "fully applied lambda (4)" "(\\x -> (\\y -> x) True) 42" int
+    , exprTestGood "unapplied lambda, polymorphic (1)" "(\\x -> True)" (forall ["a"] (fun [var "a", bool]))
+    , exprTestGood "unapplied lambda, polymorphic (2)"
+                   "(\\x y -> x) :: forall a b . a -> b -> a"
                    (forall ["a","b"] (fun [var "a", var "b", var "a"]))
-    , exprTestGood "((\\x -> x) :: forall a . a -> a) True" bool
-    , exprTestGood "((\\x y -> x) :: forall a b . a -> b -> a) True"
+    , exprTestGood "annotated, fully applied lambda" "((\\x -> x) :: forall a . a -> a) True" bool
+    , exprTestGood "annotated, partially applied lambda"
+                   "((\\x y -> x) :: forall a b . a -> b -> a) True"
                    (forall ["a"] (fun [var "a", bool]))
-    , exprTestGood "x = True; 4.2" num
-    , exprTestGood "x = True; (\\y -> y) x" bool
-    , exprTestGood "f = (\\x y -> x); f 42"
-                   (forall ["a"] (fun [var "a", int]))
-    , exprTestGood "f = (\\x y -> x); x = f 42; x"
-                   (forall ["a"] (fun [var "a", int]))
-    , exprTestGood "x :: Int" int
-    , exprTestGood "xs :: Foo a" (arr "Foo" [var "a"])
-    , exprTestGood "f :: forall a . a -> a; f 42" int
-    , exprTestGood "apply :: (Int -> Bool) -> Int -> Bool; f :: Int -> Bool; apply f 42"  bool 
-    , exprTestGood "apply :: forall a b . (a->b) -> a -> b; f :: Int -> Bool; apply f 42" bool
-    , exprTestGood "[1,2,3]" (lst int)
-    , exprTestGood "f :: forall a . a -> a; [53, f 34]" (lst int)
-    , exprTestGood "f :: forall a . a -> a; (f 53, True)" (tuple [int, bool])
-    , exprTestGood "[]" (forall ["a"] (lst (var "a")))
-    , exprTestGood "f :: [Int] -> Bool; f [1]" bool
-    , exprTestGood "f :: forall a . [a] -> Bool; f [1]" bool
-    -- * test extra space
-    , exprTestGood " 42" int
-    -- * adding signatures to declarations
-    , exprTestGood "f :: forall a . a -> a; f x = x; f 42" int
-    , exprTestGood "f :: Int -> Bool; f x = True; f 42" bool
-    , exprTestGood "f :: Int -> Bool; f x = True; f" (fun [int, bool])
-    , expectError  "f :: Int -> Bool; f x = 9999"
-                   (SubtypeError int bool)
-    -- * higher order functions
-    , exprTestGood "map :: forall a b . (a->b) -> [a] -> [b]; f :: Int -> Bool; map f [5,2]" (lst bool)
-    , exprTestGood "f = \\x -> (14,x); g = \\x f -> f x; g True f" (tuple [int, bool])
-    -- * fails to terminate when qualified type variables are not distinguished
-    -- See commit 7ffd52a
-    , exprTestGood "f :: forall a . a -> a; g :: forall a . a -> Int; g f" int
-    , exprTestGood "f :: forall a . a -> a; g :: forall b . b -> Int; g f" int
-    -- failing tests ----------------------------------------------------------
-    -- * heterogenous list
-    , exprTestBad "[1,2,True]"
-    -- * unbound variable
-    , exprTestBad "\\x -> y"
-    -- * too many arguments
-    , exprTestBad "f :: forall a . a; f Bool 12"
-    -- * arguments have the wrong type
-    , exprTestBad "abs :: Int -> Int; abs True"
-    -- * arguments to a function are monotypes
-    , expectError   "f :: forall a . a -> a; g = \\h -> (h 42, h True); g f"
-                    (SubtypeError int bool)
-    -- * test bug from 203f8c
-    , exprTestGood  "f :: forall a . a -> a; g = \\h -> (h 42, h 1234); g f"
-                    (tuple [int, int])
-    , exprTestGood  "f :: forall a . a -> a; g = \\h -> [h 42, h 1234]; g f"
-                    (lst int)
-    -- * find misuses
-    , exprTestBad "f = 14; g = \\x h -> h x; (g True) f"
-    --             ^               ^    ^ this should fail
-    -- ------------+---------------'----'
-
-    -- These should all fail, since f is not a function
-    , expectError "f = 5; g = \\x -> f x; g 12" NonFunctionDerive
-    , expectError "f = 5; g = \\h -> h 5; g f" NonFunctionDerive
-    , exprTestGood "\\f -> f 5" 
+    , exprTestGood "recursive functions are A-OK"
+                   "\\f -> f 5"
                    (forall ["a"] (fun [fun [int, var "a"], var "a"]))
-                   -- forall a . (Int -> a) -> a
+    -- applications
+    , exprTestGood "primitive variable in application" "x = True; (\\y -> y) x" bool
+    , exprTestGood "function variable in application"
+                   "f = (\\x y -> x); f 42"
+                   (forall ["a"] (fun [var "a", int]))
+    , exprTestGood "partially applied function variable in application"
+                   "f = (\\x y -> x); x = f 42; x"
+                   (forall ["a"] (fun [var "a", int]))
+    , exprTestBad "applications with too many arguments fail" "f :: forall a . a; f Bool 12" 
+    , exprTestBad "applications with mismatched types fail (1)" "abs :: Int -> Int; abs True"
+    , exprTestBad "applications with mismatched types fail (2)" "f = 14; g = \\x h -> h x; (g True) f"
+    , expectError "arguments to a function are monotypes" 
+                  "f :: forall a . a -> a; g = \\h -> (h 42, h True); g f"
+                  (SubtypeError int bool)
+    , exprTestGood  "polymorphism under lambdas (203f8c) (1)"
+                    "f :: forall a . a -> a; g = \\h -> (h 42, h 1234); g f"
+                    (tuple [int, int])
+    , exprTestGood  "polymorphism under lambdas (203f8c) (2)"
+                    "f :: forall a . a -> a; g = \\h -> [h 42, h 1234]; g f"
+                    (lst int)
+    , expectError "applications of non-funcions should fail (1)" "f = 5; g = \\x -> f x; g 12" NonFunctionDerive
+    , expectError "applications of non-funcions should fail (2)" "f = 5; g = \\h -> h 5; g f" NonFunctionDerive
+    -- binding
+    , exprTestGood "annotated variables without definition are legal" "x :: Int" int
+    , exprTestGood "unannotated variables with definition are legal" "x = 42; x" int
+    , exprTestBad "unannotated variables without definitions are illegal ('\\x -> y')" "\\x -> y"
+    -- parameterized types
+    , exprTestGood "parameterized type (n=1)" "xs :: Foo a" (arr "Foo" [var "a"])
+    , exprTestGood "parameterized type (n=2)" "xs :: Foo a b" (arr "Foo" [var "a", var "b"])
+    , exprTestGood "nested parameterized type" "xs :: Foo (Bar a) [b]"
+                   (arr "Foo" [arr "Bar" [var "a"], arr "List" [var "b"]])
+    -- type signatures and higher-order functions
+    , exprTestGood "type signature: identity function" "f :: forall a . a -> a; f 42" int
+    , exprTestGood "type signature: apply function with primitives"
+                   "apply :: (Int -> Bool) -> Int -> Bool; f :: Int -> Bool; apply f 42"  bool
+    , exprTestGood "type signature: generic apply function"
+                   "apply :: forall a b . (a->b) -> a -> b; f :: Int -> Bool; apply f 42" bool
+    , exprTestGood "type signature: map" "map :: forall a b . (a->b) -> [a] -> [b]; f :: Int -> Bool; map f [5,2]" (lst bool)
+    -- shadowing
+    , exprTestGood "name shadowing in lambda expressions" "f = \\x -> (14,x); g = \\x f -> f x; g True f" (tuple [int, bool])
+    , exprTestGood "shadowed qualified type variables (7ffd52a)"     "f :: forall a . a -> a; g :: forall a . a -> Int; g f" int
+    , exprTestGood "non-shadowed qualified type variables (7ffd52a)" "f :: forall a . a -> a; g :: forall b . b -> Int; g f" int
+    -- lists
+    , exprTestGood "list of primitives" "[1,2,3]" (lst int)
+    , exprTestGood "list containing an applied variable" "f :: forall a . a -> a; [53, f 34]" (lst int)
+    , exprTestGood "empty list" "[]" (forall ["a"] (lst (var "a")))
+    , exprTestGood "list in function signature and application" "f :: [Int] -> Bool; f [1]" bool
+    , exprTestGood "list in generic function signature and application" "f :: forall a . [a] -> Bool; f [1]" bool
+    , exprTestBad "failure on heterogenous list" "[1,2,True]"
+    -- tuples
+    , exprTestGood "tuple of primitives" "(4.2, True)" (arr "Tuple2" [num, bool])
+    , exprTestGood "tuple containing an applied variable" "f :: forall a . a -> a; (f 53, True)" (tuple [int, bool])
+    -- extra space
+    , exprTestGood "leading space" " 42" int
+    , exprTestGood "trailing space" "42 " int
+    -- adding signatures to declarations
+    , exprTestGood "declaration with a signature (1)" "f :: forall a . a -> a; f x = x; f 42" int
+    , exprTestGood "declaration with a signature (2)" "f :: Int -> Bool; f x = True; f 42" bool
+    , exprTestGood "declaration with a signature (3)" "f :: Int -> Bool; f x = True; f" (fun [int, bool])
+    , expectError  "primitive type mismatch should raise error" "f :: Int -> Bool; f x = 9999"
+                   (SubtypeError int bool)
 
-    -- tests modules ----------------------------------------------------------
-    , exprTestGood "module Main {[1,2,3]}" (lst int)
-    , (flip exprTestGood) (lst int) $ T.unlines
+    -- tests modules
+    , exprTestGood "basic Main module" "module Main {[1,2,3]}" (lst int)
+    , (flip $ exprTestGood "import/export") (lst int) $ T.unlines
         [ "module Foo {export x; x = 42};"
         , "module Bar {export f; f :: forall a . a -> [a]};"
         , "module Main {import Foo (x); import Bar (f); f x}"
         ]
-    , (flip expectError) (BadImport (MV "Foo") (EV "x")) $ T.unlines
-        [ "module Foo {export y; y = 42};"
-        , "module Main {import Foo (x); x}"
-        ]
-    , (flip expectError) CannotImportMain $ T.unlines
+    , (flip $ expectError "fail on import of Main") CannotImportMain $ T.unlines
         [ "module Main {export x; x = 42};"
         , "module Foo {import Main (x)}"
         ]
-    , (flip expectError) CyclicDependency $ T.unlines
+    , (flip $ expectError "fail on import of non-existing variable") (BadImport (MV "Foo") (EV "x")) $ T.unlines
+        [ "module Foo {export y; y = 42};"
+        , "module Main {import Foo (x); x}"
+        ]
+    , (flip $ expectError "fail on cyclic dependency") CyclicDependency $ T.unlines
         [ "module Foo {import Bar (y); export x; x = 42};"
         , "module Bar {import Foo (x); export y; y = 88}"
         ]
-    , (flip expectError) (MultipleModuleDeclarations (MV "Foo")) $ T.unlines
+    , (flip $ expectError "fail on redundant module declaration") (MultipleModuleDeclarations (MV "Foo")) $ T.unlines
         [ "module Foo {x = 42};"
         , "module Foo {x = 88}"
         ]
-    , (flip expectError) (SelfImport (MV "Foo")) $ T.unlines
+    , (flip $ expectError "fail on self import") (SelfImport (MV "Foo")) $ T.unlines
         [ "module Foo {import Foo (x); x = 42}"
         ]
-    , (flip expectError) (BadImport (MV "Foo") (EV "x")) $ T.unlines
+    , (flip $ expectError "fail on import of non-exported variable") (BadImport (MV "Foo") (EV "x")) $ T.unlines
         [ "module Foo {x = 42};"
         , "module Main {import Foo (x); x}"
         ]
-    -- test realization integration -------------------------------------------
-    , (flip exprTestGood) int $ T.unlines
+    -- test realization integration
+    , (flip $ exprTestGood "a realization can be defined following general type signature") int $ T.unlines
         [ "f :: Int -> Int;"
         , "f r :: integer -> integer;"
         , "f 44"
         ]
-    -- This looks bad, but for now I allow it. I may add more strictness later 
-    , (flip exprTestGood) int $ T.unlines
+    , (flip $ exprTestGood "realizations can map one general type to multiple specific ones ") int $ T.unlines
         [ "f :: Int -> Int;"
-        , "f r :: integer -> string;" 
+        , "f r :: integer -> numeric;"
+        , "f 44"
+        ]
+    , (flip $ exprTestGood "realizations can map multiple general type to one specific one") (var "Nat") $ T.unlines
+        [ "f :: Int -> Nat;"
+        , "f r :: integer -> integer;"
+        , "f 44"
+        ]
+    , (flip $ exprTestGood "multiple realizations for different languages can be defined") int $ T.unlines
+        [ "f :: Int -> Int;"
+        , "f r :: integer -> integer;"
         , "f c :: int -> int;"
         , "f 44"
         ]
-    -- can also have more than one realization for a given language
-    , (flip exprTestGood) int $ T.unlines
+    , (flip $ exprTestGood "the order of general signatures and realizations does not matter (1)") int $ T.unlines
+        [ "f r :: integer -> integer;"
+        , "f :: Int -> Int;"
+        , "f c :: int -> int;"
+        , "f 44"
+        ]
+    , (flip $ exprTestGood "the order of general signatures and realizations does not matter (2)") int $ T.unlines
+        [ "f r :: integer -> integer;"
+        , "f c :: int -> int;"
+        , "f :: Int -> Int;"
+        , "f 44"
+        ]
+    , (flip $ exprTestGood "multiple realizations for a single language can be defined") int $ T.unlines
         [ "f :: Int -> Int;"
-        , "f r :: integer -> string;" 
+        , "f r :: integer -> integer;"
         , "f r :: int -> int;"
         , "f 44"
         ]
-    -- but the number of arguments must be conserved
-    , (flip expectError) BadRealization $ T.unlines
+    , (flip $ expectError "a general signature must be given") (UnboundVariable (EV "f")) $ T.unlines
+        [ "f r :: integer -> integer;"
+        , "f 44"
+        ]
+    , (flip $ exprTestGood "no general type signature is required if it can be inferred") int $ T.unlines
+        [ "f r :: integer -> integer;"
+        , "f = \\x -> 42;"
+        , "f 44"
+        ] 
+    , (flip $ expectError "arguments number in realizations must equal the general case (1)") BadRealization $ T.unlines
         [ "f :: Int -> String -> Int;"
         , "f r :: integer -> integer;"
         , "f 44"
         ]
-    -- the general type must be declared
-    , (flip expectError) (UnboundVariable (EV "f")) $ T.unlines
-        [ "f r :: integer -> integer;"
-        , "f 44"
-        ]
-    -- no general type signature is required if it can be inferred
-    , (flip exprTestGood) int $ T.unlines
-        [ "f r :: integer -> integer;"
-        , "f = \\x -> 42;"
-        , "f 44"
-        ]
-    , (flip expectError) (BadRealization) $ T.unlines
+    , (flip $ expectError "arguments number in realizations must equal the general case (2)") (BadRealization) $ T.unlines
         [ "f   :: Int -> Int;"
         , "f r :: integer -> integer -> string;"
         , "f 44"
         ]
-    -- internal ---------------------------------------------------------------
-    , exprTestFull "f :: forall a . a -> Bool; f 42"
+    -- internal
+    , exprTestFull "every sub-expression should be annotated in output"
+                   "f :: forall a . a -> Bool; f 42"
                    "f :: forall a . a -> Bool; (((f :: Int -> Bool) (42 :: Int)) :: Bool)"
   ]
